62d4ee68a0e2d1eec85922301cfae46731242a27
Прохорова Юлия <y.prokhorova@glowbyteconsulting.com>
2024-10-12 16:53:36 +0300

    fix release

--- build-win.bat
+++ build-win.bat
@@ -1,1 +1,10 @@
-pyinstaller main.py -n "Therometer-MI" --console --onefile
+@echo off
+setlocal
+
+if "%1"=="" (
+    exit /b
+) else (
+    set "Tag=%1"
+)
+
+pyinstaller main.py -n "Therometer-MI_%Tag%" --console --onefile
